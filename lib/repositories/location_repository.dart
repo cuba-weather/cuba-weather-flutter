e0e6f967e8e39d5990914df30aec1496bb21b149
leynier <leynier41@gmail.com>
2020-01-09 00:50:52 -0500

    Sorted locations when Location repository is instantiated

--- lib/repositories/location_repository.dart
+++ lib/repositories/location_repository.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 import 'package:cuba_weather/repositories/data_providers/data_providers.dart';
 
 class LocationRepository {
+  LocationRepository() {
+    locations.sort((a, b) => b.length - a.length);
+  }
+
   Future<String> getLocation(String locationQuery) async {
     var locationQueryLowerCase = locationQuery.toLowerCase();
     for (var location in locations) {
