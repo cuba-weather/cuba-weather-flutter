dd7f577b1f7c45ed0c55f7945401a120b7ccd42b
leynier <leynier41@gmail.com>
2020-01-13 17:25:04 -0500

    Implement MVP

--- lib/main.dart
+++ lib/main.dart
@@ -28,47 +28,93 @@ class MyHomePage extends StatefulWidget {
 class MyHomePageState extends State<MyHomePage> {
   final _cubaWeather = CubaWeather();
   final _textEditingController = TextEditingController();
-  String _text;
+  bool _loading = false;
 
-  void _search() {
-    log('Searching...');
-    _cubaWeather.get(_textEditingController.text, suggestion: true).then((weather) {
-      setState(() {
-        _text = weather.toString();
-        log(_text);
-      });
-    }).catchError((error) {
-      setState(() {
-        if (error is InvalidLocationException) {
-          _text = error.message;
-        } else if (error is BadRequestException) {
-          _text = error.message;
-        } else {
-          _text = error.toString();
-        }
-        log(_text);
-      });
+  Widget _alertDialog(context, WeatherModel weather) {
+    return AlertDialog(
+      title: Text(weather.cityName),
+      content: Column(
+        children: <Widget>[
+          Row(children: <Widget>[
+            Text('Temperatura: '),
+            Text('${weather.temp}')
+          ]),
+          Row(children: <Widget>[Text('Fecha: '), Text('${weather.dt.date}')]),
+          Row(children: <Widget>[
+            Text('Humedad: '),
+            Text('${weather.humidity}')
+          ]),
+          Row(children: <Widget>[
+            Text('Presión: '),
+            Text('${weather.pressure}')
+          ]),
+          Row(children: <Widget>[
+            Text('Viento: '),
+            Text('${weather.windstring}')
+          ]),
+          Row(children: <Widget>[
+            Text('Descripción: '),
+            Text('${weather.descriptionWeather}')
+          ]),
+          Row(children: <Widget>[Image.network(weather.iconWeather)]),
+        ],
+      ),
+    );
+  }
+
+  void _search() async {
+    setState(() {
+      _loading = true;
     });
+    log('Searching...');
+    try {
+      var text = _textEditingController.text;
+      var weather = await _cubaWeather.get(text, suggestion: true);
+      return showDialog(
+          context: context,
+          builder: (context) => _alertDialog(context, weather));
+    } on InvalidLocationException catch (error) {
+      log(error.message);
+    } on BadRequestException catch (error) {
+      log(error.message);
+    } catch (error) {
+      log(error);
+    } finally {
+      setState(() {
+        _loading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_text ?? ''),
-            TextField(controller: _textEditingController),
-          ],
+    if (_loading) {
+      return Scaffold(
+          appBar: AppBar(title: Text(widget.title)),
+          body: Center(child: CircularProgressIndicator()));
+    } else {
+      return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 32, right: 32),
+                child: TextField(
+                  controller: _textEditingController,
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _search,
-        tooltip: 'Search',
-        child: Icon(Icons.search),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: _search,
+          tooltip: 'Search',
+          child: Icon(Icons.search),
+        ),
+      );
+    }
   }
 }
