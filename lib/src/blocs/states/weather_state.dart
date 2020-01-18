aa337612da67319986fa5d64b61361e933970c8e
leynier <leynier41@gmail.com>
2020-01-17 21:45:47 -0500

    Implement states

---
+++ lib/src/blocs/states/weather_state.dart
@@ -0,0 +1,26 @@
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+import 'package:cuba_weather_dart/cuba_weather_dart.dart';
+
+abstract class WeatherState extends Equatable {
+  const WeatherState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class WeatherEmpty extends WeatherState {}
+
+class WeatherLoading extends WeatherState {}
+
+class WeatherLoaded extends WeatherState {
+  final WeatherModel weather;
+
+  const WeatherLoaded({@required this.weather}) : assert(weather != null);
+
+  @override
+  List<Object> get props => [weather];
+}
+
+class WeatherError extends WeatherState {}
