d8be5e0f5358e48452dd8270c5dcc0814acb3878
leynier <leynier41@gmail.com>
2020-01-17 21:46:18 -0500

    Implement blocs

---
+++ lib/src/blocs/weather_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
+import 'package:cuba_weather_dart/cuba_weather_dart.dart';
+
+import 'package:cuba_weather/src/blocs/blocs.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final CubaWeather api;
+
+  WeatherBloc({@required this.api}) : assert(api != null);
+
+  @override
+  WeatherState get initialState => WeatherEmpty();
+
+  @override
+  Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
+    if (event is FetchWeather) {
+      yield WeatherLoading();
+      try {
+        final weather = await api.get(event.location, suggestion: true);
+        yield WeatherLoaded(weather: weather);
+      } catch (_) {
+        yield WeatherError();
+      }
+    }
+  }
+}
