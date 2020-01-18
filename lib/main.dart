8aa959414e7b0ce691d3af635c0704cc450d9d9a
leynier <leynier41@gmail.com>
2020-01-17 23:04:32 -0500

    Implement new app widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+
+import 'package:bloc/bloc.dart';
+import 'package:cuba_weather_dart/cuba_weather_dart.dart';
 
 import 'package:cuba_weather/src/app.dart';
 
-void main() => runApp(App());
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(App(api: CubaWeather()));
+}
+
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log(transition.toString());
+  }
+}
