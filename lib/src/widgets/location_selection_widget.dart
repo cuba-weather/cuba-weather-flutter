8c57604cade3d4b71ae6f04d6ed4324c3e55c071
leynier <leynier41@gmail.com>
2020-01-17 23:01:52 -0500

    Implement location selection widget

---
+++ lib/src/widgets/location_selection_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class LocationSelectionWidget extends StatefulWidget {
+  @override
+  State<LocationSelectionWidget> createState() =>
+      _LocationSelectionWidgetState();
+}
+
+class _LocationSelectionWidgetState extends State<LocationSelectionWidget> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Seleccionar localización'),
+      ),
+      body: Form(
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: TextFormField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    labelText: 'Localización',
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                Navigator.pop(context, _textController.text);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
