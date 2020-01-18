0c1caa7b9b397122124e0e6fdfbe85d7dd5e06ec
leynier <leynier41@gmail.com>
2020-01-17 23:03:46 -0500

    Export widgets

---
+++ lib/src/widgets/widgets.dart
@@ -0,0 +1,7 @@
+export 'combined_weather_widget.dart';
+export 'gradient_container_widget.dart';
+export 'last_updated_widget.dart';
+export 'name_location_widget.dart';
+export 'location_selection_widget.dart';
+export 'data_widget.dart';
+export 'weather_widget.dart';
