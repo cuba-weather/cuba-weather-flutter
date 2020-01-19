e5872dae40876f5804291bf8a63fb13dd0dc9b28
leynier <leynier41@gmail.com>
2020-01-19 01:13:49 -0500

    Improve information widget

--- lib/src/widgets/information_widget.dart
+++ lib/src/widgets/information_widget.dart
@@ -40,98 +40,64 @@ class InformationWidgetState extends State<InformationWidget> {
         color: Colors.blue,
         child: Column(
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 100),
-              child: Center(
-                child: Text(
-                  '$appName',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      '$appName',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 5),
-              child: Center(
-                child: Text(
-                  '$version',
-                  style: TextStyle(
-                    color: Colors.white,
+                  Center(
+                    child: Text(
+                      '$version',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 30, left: 100, right: 100),
-              child: Center(
-                child: Image.asset(
-                  'images/logo.png',
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 50),
-              child: Center(
-                child: Text(
-                  'Desarrollado por:',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                  Center(
+                    child: Image.asset(
+                      'images/logo.png',
+                      width: 200,
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 90),
-              child: GFButton(
-                text: 'Leynier Gutiérrez González',
-                textColor: Colors.blue,
-                color: Colors.white,
-                size: GFSize.large,
-                shape: GFButtonShape.pills,
-                fullWidthButton: true,
-                onPressed: () async {
-                  const url = 'https://leynier.github.io';
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  } else {
-                    throw 'Could not launch $url';
-                  }
-                },
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 50),
-              child: Center(
-                child: Text(
-                  'Dudas, problemas o comentarios al:',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      'Dudas, problemas o comentarios a:',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 90),
-              child: GFButton(
-                text: 'Repositorio en GitHub',
-                textColor: Colors.blue,
-                color: Colors.white,
-                size: GFSize.large,
-                shape: GFButtonShape.pills,
-                fullWidthButton: true,
-                onPressed: () async {
-                  const url = 'https://github.com/leynier/cuba-weather-flutter';
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  } else {
-                    throw 'Could not launch $url';
-                  }
-                },
+                  Center(
+                    child: Text(
+                      'leynier41@gmail.com',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
