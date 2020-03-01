6e64b59989a9f5a77c0244706efebd49e387f894
leynier <leynier41@gmail.com>
2020-03-01 01:18:19 -0500

    Add gradient to splash screen

--- lib/src/pages/splash_page.dart
+++ lib/src/pages/splash_page.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:developer';
+
 import 'package:cuba_weather/src/pages/home_page.dart';
 import 'package:cuba_weather/src/pages/pages.dart';
 import 'package:cuba_weather/src/utils/constants.dart';
+import 'package:cuba_weather/src/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:cuba_weather/src/widgets/responsive_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -22,6 +24,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   ResponsiveScreen size;
+
   @override
   initState() {
     super.initState();
@@ -34,61 +37,72 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     size = ResponsiveScreen(MediaQuery.of(context).size);
     return Scaffold(
-        body: Container(
-      color: Constants.clr_gradient_max,
-      child: Center(
+      body: GradientContainerWidget(
+        color: Colors.blue,
+        child: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-              width: size.getWidthPx(200),
-              height: size.getWidthPx(200),
-              child: Image.asset(Constants.appLogo)),
-          Text(
-            Constants.appName,
-            style: TextStyle(
-                fontFamily: Constants.fontFamily,
-                fontWeight: FontWeight.w500,
-                fontSize: 26,
-                color: Colors.white),
-            textAlign: TextAlign.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: size.getWidthPx(200),
+                height: size.getWidthPx(200),
+                child: Image.asset(Constants.appLogo),
+              ),
+              Text(
+                Constants.appName,
+                style: TextStyle(
+                  fontFamily: Constants.fontFamily,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 26,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                Constants.appSlogan.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: Constants.fontFamily,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
-          Text(
-            Constants.appSlogan.toUpperCase(),
-            style: TextStyle(
-                fontFamily: Constants.fontFamily,
-                fontWeight: FontWeight.w300,
-                fontSize: 15,
-                color: Colors.white),
-            textAlign: TextAlign.center,
-          ),
-        ],
-      )),
-    ));
+        ),
+      ),
+    );
   }
 
   Future navigateFromSplash() async {
-    String isOnBoard = '0';
+    bool isOnBoard = false;
     try {
       var prefs = await SharedPreferences.getInstance();
-      isOnBoard = prefs.getString(Constants.isOnBoard);
+      isOnBoard = prefs.getBool(Constants.isOnBoard);
     } catch (e) {
       log(e.toString());
     }
     print("isOnBoard  $isOnBoard");
-    if (isOnBoard == null || isOnBoard == "0") {
+    if (isOnBoard == null || !isOnBoard) {
       //Navigate to OnBoarding Screen.
       Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (context) => OnBoardingPage(
-                  initialMunicipality: widget.initialMunicipality)));
+        context,
+        MaterialPageRoute(
+          builder: (context) => OnBoardingPage(
+            initialMunicipality: widget.initialMunicipality,
+          ),
+        ),
+      );
     } else {
       Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  HomePage(initialMunicipality: widget.initialMunicipality)));
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(
+            initialMunicipality: widget.initialMunicipality,
+          ),
+        ),
+      );
     }
   }
 }
