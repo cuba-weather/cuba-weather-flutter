e9c92db6e53b4c11dfdf152e2d93e44cdcd3e991
leynier <leynier41@gmail.com>
2020-03-08 12:01:03 -0400

    Fix errors

--- lib/src/widgets/combined_weather_widget.dart
+++ lib/src/widgets/combined_weather_widget.dart
@@ -10,22 +10,28 @@ class CombinedWeatherWidget extends StatelessWidget {
       : assert(weather != null),
         super(key: key);
 
+  getChildren() {
+    var haveToday = weather.getTodayForecast() != null;
+    List<Widget> result = [
+      DividerWidget(),
+      Container(
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        child: ActualStateWidget(weather: weather),
+      ),
+      DividerWidget(),
+      ForecastWidget(weather: weather),
+    ];
+    if (haveToday) {
+      result.insert(2, DividerWidget());
+      result.insert(3, TodayForecastWidget(weather: weather));
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        DividerWidget(),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 30),
-          child: ActualStateWidget(weather: weather),
-        ),
-        weather.getForecasts() == null ? null : DividerWidget(),
-        weather.getForecasts() == null
-            ? null
-            : TodayForecastWidget(weather: weather),
-        DividerWidget(),
-        ForecastWidget(weather: weather),
-      ],
+      children: getChildren(),
     );
   }
 }
